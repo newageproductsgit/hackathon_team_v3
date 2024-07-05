2b8f0ce6cdad3fcf5c20bd1a5f8288fba5650880
njoshi10 <njoshi@newageproducts.com>
2024-07-05 20:16:27 +0530

    css fixed

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import "../styles/Home.module.css"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "../styles/Home.module.css";
 export default function CreateRoom({ socket, onRoomCreated }) {
-  const [name, setName] = useState('')
+  const [name, setName] = useState("");
 
   const createRoom = () => {
-    const newRoomId = uuidv4()
-    socket.emit('create-room', { roomId: newRoomId, userName: name })
-    onRoomCreated(newRoomId)
-  }
+    const newRoomId = uuidv4();
+    socket.emit("create-room", { roomId: newRoomId, userName: name });
+    onRoomCreated(newRoomId);
+  };
 
   return (
     <div>
       <input
-      className="create-input"
+        className="create-input"
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Enter Your Name"
       />
-      <button className='button-1 marginLeft' onClick={createRoom}>Create Room</button>
+      <button className="button-1 marginLeft" onClick={createRoom}>
+        Create Room
+      </button>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import CreateRoom from "../components/CreateRoom";
 import JoinRoom from "../components/JoinRoom";
 import { useRouter } from "next/router";
 
-
-export default function Home({socket}) {
+export default function Home({ socket }) {
   const { asPath } = useRouter();
   const [roomId, setRoomId] = useState("");
   const router = useRouter();
@@ -33,24 +32,25 @@ export default function Home({socket}) {
           <div className="info-box">
             <p>Create Room</p>
             <CreateRoom socket={socket} onRoomCreated={handleRoomCreated} />
-              {roomId && <p>Room created! ID: {roomId}</p>}
+            {roomId && <p>Room created! ID: {roomId}</p>}
             <div className="cta-container">
-              
               {/* <hr /> */}
               {/* <h2>Join a Room</h2> */}
-              
             </div>
-             <h2>Join a Room</h2>
+            <h2>Join a Room</h2>
             <JoinRoom socket={socket} onRoomJoined={handleRoomJoined} />
           </div>
-
-          <div className="image-container">
+          <p>
+            <b>OR</b>
+          </p>
+          <button className="single_p_button">Play Singleplayer</button>
+          {/* <div className="image-container">
             <img
               src="/assets/img2.webp"
               alt="KBC Host"
               style={{ width: "500px" }}
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
