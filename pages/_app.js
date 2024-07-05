a335845ef99549cf704c8eef00f1fc3b42dae88f
Sourabh Mahato <79437481+smahato-nap@users.noreply.github.com>
2024-07-05 18:32:45 +0530

    added room join and create

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    const initSocket = async () => {
+      await fetch('/api/socket')
+      const newSocket = io()
+      setSocket(newSocket)
+    }
+
+    initSocket()
+
+    return () => {
+      if (socket) socket.disconnect()
+    }
+  }, [])
+
+  return <Component {...pageProps} socket={socket} />
 }
+
+export default MyApp
