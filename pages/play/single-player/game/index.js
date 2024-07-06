ebe1670265110efe8e1934d405d9b7e5871057fb
njoshi10 <njoshi@newageproducts.com>
2024-07-06 13:59:26 +0530

    level building

--- pages/play/single-player/game/index.js
+++ pages/play/single-player/game/index.js
@@ -2,37 +2,29 @@ import React, { useState, useEffect, useRef } from "react";
 import styles from "../../question.module.css";
 import { useRouter } from "next/router";
 
-const disabled = {
-  minHeight: "50px",
-  color: "#ffffff",
-  padding: "10px 20px",
-  fontSize: "0.9em",
-  cursor: "pointer",
-  border: "2px solid #ffd700",
-  position: "relative",
-  textAlign: "left",
-  borderRadius: "50px",
-  boxShadow: "0 0 10px #ffd700",
-  transition: "all 0.3s ease",
-  color: "#000",
-  pointerEvent: "none",
-  backgroundColor: "#dbc4c4",
-  cursor: "defult",
-};
-
-const GameQuestionContainer = () => {
-  const [timeLeft, setTimeLeft] = useState(30);
+const GameQuestionContainer = ({ questions }) => {
+  const [timeLeft, setTimeLeft] = useState(60);
   const [showPopup, setShowPopup] = useState(false);
   const [showDisable, setShowDisable] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [options, setOptions] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [gameLevel, setGameLevel] = useState(0);
+  const [gamePrize, setGamePrize] = useState(0);
+  const [questionsAsked, setQuestionsAsked] = useState([]);
   const router = useRouter();
   const audioRef = useRef(null);
+
+  const easyQuestions = questions[0]?.easy || [];
+  const mediumQuestions = questions[1]?.medium || [];
+  const hardQuestions = questions[2]?.hard || [];
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft((prevTime) => {
         if (prevTime <= 1) {
-          // clearInterval(timer);
           setShowPopup(true);
+          clearInterval(timer);
         }
         return prevTime > 0 ? prevTime - 1 : 0;
       });
@@ -41,14 +33,73 @@ const GameQuestionContainer = () => {
     return () => clearInterval(timer);
   }, []);
 
-  const handleDisable = () => {
-    setShowDisable(true);
+  useEffect(() => {
+    fetchRandomQuestion();
+  }, [gameLevel]);
+
+  const fetchRandomQuestion = () => {
+    let questionSet;
+    if (gameLevel < 5) {
+      questionSet = easyQuestions;
+    } else if (gameLevel < 10) {
+      questionSet = mediumQuestions;
+    } else {
+      questionSet = hardQuestions;
+    }
+  
+    if (!questionSet || questionsAsked.length >= questionSet.length) {
+      console.error("No more questions available.");
+      return;
+    }
+  
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * questionSet.length);
+    } while (questionsAsked.includes(randomIndex));
+  
+    const newQuestion = questionSet[randomIndex];
+    if (newQuestion) {
+      setQuestion(newQuestion.question);
+      setOptions(newQuestion.options);
+      setAnswer(newQuestion.answer);
+      setQuestionsAsked([...questionsAsked, randomIndex]);
+    } else {
+      console.error("No valid question found at random index.");
+    }
   };
+  
+
   const handleRestart = () => {
-    router.push("/");
-    // setShowPopup(false);
-    // setTimeLeft(30);
+    setTimeLeft(60);
+    setShowPopup(false);
+    setShowDisable(false);
+    setGameLevel(0);
+    setGamePrize(0);
+    setQuestionsAsked([]);
+    fetchRandomQuestion();
   };
+
+  const handleOptionClick = (selectedAnswer) => {
+    if (selectedAnswer === answer) {
+      setGameLevel((prevLevel) => prevLevel + 1);
+      setGamePrize((prevPrize) => {
+        switch (prevPrize) {
+          case 0:
+            return 1000;
+          case 1000:
+            return 2000;
+          // Add more cases as per your game's prize structure
+          default:
+            return prevPrize;
+        }
+      });
+      setTimeout(fetchRandomQuestion, 1000); // Delay to prevent rapid state updates
+    } else {
+      // Handle incorrect answer logic if needed
+      // For example, end game or show modal
+    }
+  };
+
   useEffect(() => {
     const playAudio = async () => {
       if (audioRef.current) {
@@ -68,89 +119,91 @@ const GameQuestionContainer = () => {
       }
     };
   }, []);
-  return (
-    <div className={styles.gameContainer}>
-      <audio
-        ref={audioRef}
-        src="/assets/kbc-awesome-5410.mp3"
-        controls
-        hidden
-      />
-      <div className={styles.timer}>Time left - {timeLeft} seconds</div>
-      <h1 className={styles.title}>Question 1/15</h1>
-      <div className={styles.prizeLevel}>Price won: $1,000,000</div>
-      <div className={styles.content}>
-        <main>
-          <div className={styles.questionBox}>
-            <p>What is the capital city of Australia?</p>
-          </div>
-          <div className={styles.answersGrid}>
-            {!showDisable ? (
-              <>
-                <button className={styles.answerBtn}>A: Sydney</button>
-                <button className={styles.answerBtn}>B: Melbourne</button>
-                <button className={styles.answerBtn}>C: Canberra</button>
-                <button className={styles.answerBtn}>D: Perth</button>
-              </>
-            ) : (
-              <>
-                <button className={styles.answerBtn}>A: Sydney</button>
-                <button className={"disabled"} style={disabled}>
-                  B: Melbourne
-                </button>
-                <button className={"disabled"} style={disabled}>
-                  C: Canberra
-                </button>
-                <button className={styles.answerBtn}>D: Perth</button>
-              </>
-            )}
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              fontWeight: "bold",
-              fontSize: "1.5rem",
-              marginTop: "100px",
-              marginBottom: "50px",
-            }}
-          >
-            Got stuck? Use your life lines!
-          </div>
-          <div className={styles.lifelines}>
-            <button
-              className={styles.lifelineBtn}
-              data-hover-text="Removes two wrong answers"
-              onClick={handleDisable}
-            >
-              50:50
-            </button>
-            <button
-              className={styles.lifelineBtn}
-              data-hover-text="Call a friend for help"
-            >
-              Phone a Friend
-            </button>
-            <button
-              className={styles.lifelineBtn}
-              data-hover-text="Ask the audience for help"
-            >
-              Ask the Audience
-            </button>
-          </div>
-        </main>
 
-        {showPopup && (
-          <div aria-hidden="true" className={styles.overlay}>
-            <div className={styles.centeredDiv}>
-              <button onClick={handleRestart} className="button-1">
-                Restart Game
+  return (
+    <>
+      <div className={`${styles.gameContainer} ${showPopup ? styles.blur : ""}`}>
+        <audio ref={audioRef} src="/assets/kbc-awesome-5410.mp3" controls hidden />
+        <div className={styles.timer}>Time left - {timeLeft} seconds</div>
+        <h1 className={styles.title}>Question {gameLevel + 1}/15</h1>
+        <div className={styles.prizeLevel}>Prize won: ${gamePrize}</div>
+        <div className={styles.content}>
+          <main>
+            <div className={styles.questionBox}>
+              <p>{question}</p>
+            </div>
+            <div className={styles.answersGrid}>
+              {options.map((option, index) => (
+                <button
+                  key={index}
+                  className={`${styles.answerBtn} ${
+                    index % 2 === 0
+                      ? styles.answerBtn_left
+                      : styles.answerBtn_right
+                  }`}
+                  onClick={() => handleOptionClick(option)}
+                  disabled={showDisable}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                fontWeight: "bold",
+                fontSize: "1.5rem",
+                marginTop: "100px",
+                marginBottom: "50px",
+              }}
+            >
+              Got stuck? Use your life lines!
+            </div>
+            <div className={styles.lifelines}>
+              <button
+                className={styles.lifelineBtn}
+                data-hover-text="Removes two wrong answers"
+                onClick={() => setShowDisable(true)}
+                disabled={showDisable}
+              >
+                50:50
+              </button>
+              <button className={styles.lifelineBtn} data-hover-text="Call a friend for help">
+                Phone a Friend
+              </button>
+              <button className={styles.lifelineBtn} data-hover-text="Ask the audience for help">
+                Ask the Audience
               </button>
             </div>
-          </div>
-        )}
+          </main>
+        </div>
       </div>
-    </div>
+      {showPopup && (
+        <div aria-hidden="true" className={styles.overlay}>
+          <div className={styles.centeredDiv}>
+            <div className={styles.modalHeader}>
+              <p className={styles.p1}>Time is up!</p>
+              <p className={styles.p2}>Better luck next time!</p>
+              <p className={styles.p3}>Prize won: ${gamePrize}</p>
+            </div>
+            <button onClick={handleRestart} className="button-1">
+              Restart Game
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 export default GameQuestionContainer;
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/questions/list.json"); // Adjust the URL as necessary
+  const questions = await res.json();
+  return {
+    props: {
+      questions,
+    },
+  };
+}
