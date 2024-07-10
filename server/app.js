29066d4d4da05d30d95e133761732f802eac4e03
njoshi10 <njoshi@newageproducts.com>
2024-07-10 13:39:27 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
       "http://localhost:3001",
       "http://localhost:3000",
       "https://hackathon-team-v3-9y6k.vercel.app/",
-      "https://hackathon-team-v3-9y6k.vercel.app/",
+      "https://hackathon-team-v3-lyd9.vercel.app/",
     ],
     methods: ["GET", "POST"],
     transports: ["websocket"],
