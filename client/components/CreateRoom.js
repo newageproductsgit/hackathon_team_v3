09f101cc698c343f914b662382d71a62dc020c2b
njoshi10 <njoshi@newageproducts.com>
2024-07-11 00:18:24 +0530

    added local storage

--- client/components/CreateRoom.js
+++ client/components/CreateRoom.js
@@ -3,15 +3,16 @@ import { v4 as uuidv4 } from "uuid";
 import "../styles/Home.module.css";
 import { useRouter } from "next/router";
 
-export default function CreateRoom({ socket,roomUsers}) {
-  const [roomName, setRoomName] = useState('')
+export default function CreateRoom({ socket, roomUsers }) {
+  const [roomName, setRoomName] = useState("");
   const [name, setName] = useState("");
-  const router=useRouter()
-  console.log(router)
+  const router = useRouter();
+  console.log(router);
   const createRoom = (e) => {
     e.preventDefault();
+    window?.localStorage.setItem("kbc_name", name);
     socket.emit("join-room", { room: roomName, name });
-      router.push(`/lobby?roomid=${roomName}`)
+    router.push(`/lobby?roomid=${roomName}`);
   };
 
   return (
@@ -20,17 +21,21 @@ export default function CreateRoom({ socket,roomUsers}) {
         className="create-input"
         type="text"
         value={roomName}
-        onChange={(e) =>setRoomName(e.target.value)}
+        onChange={(e) => setRoomName(e.target.value)}
         placeholder="Enter Room Name"
       />
-       <input
-          type="text"
-          className="create-input"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Enter Your Name"
-        />
-      <button className="button-1 marginLeft" type='submit' style={{marginTop:'20px'}}>
+      <input
+        type="text"
+        className="create-input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Your Name"
+      />
+      <button
+        className="button-1 marginLeft"
+        type="submit"
+        style={{ marginTop: "20px" }}
+      >
         Create/Join Room
       </button>
     </form>
